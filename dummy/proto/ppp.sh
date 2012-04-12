3bafc1916e60745ebef60f1de678195824a1f351
Felix Fietkau <nbd@openwrt.org>
2012-04-12 15:56:36 +0200

    add a default route in the dummy ppp.sh implementation to debug default route handling issues

--- dummy/proto/ppp.sh
+++ dummy/proto/ppp.sh
@@ -39,6 +39,7 @@ proto_pppoe_setup() {
 	proto_init_update ppp0 1
 	proto_add_ipv4_address "192.168.2.1" 32
 	proto_add_dns_server "192.168.2.2"
+	proto_add_ipv4_route "0.0.0.0" 0 192.168.2.2
 	proto_add_data
 	json_add_string "ppp-type" "pppoe"
 	proto_close_data
