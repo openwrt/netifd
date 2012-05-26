478d49f227dfed9c1b2178107bcb06221956e8f8
Felix Fietkau <nbd@openwrt.org>
2012-05-26 17:56:41 +0200

    vlist is now part of libubox

--- utils.h
+++ utils.h
@@ -18,6 +18,7 @@
 #include <libubox/avl.h>
 #include <libubox/avl-cmp.h>
 #include <libubox/blobmsg.h>
+#include <libubox/vlist.h>
 
 #ifndef __OPTIMIZE__
 #define BUILD_BUG_ON(condition) ((void)sizeof(char[1 - 2*!!(condition)]))
@@ -39,47 +40,6 @@ static inline bool blobmsg_get_bool_default(struct blob_attr *attr, bool val)
 }
 
 #define __init __attribute__((constructor))
-
-struct vlist_tree;
-struct vlist_node;
-
-typedef void (*vlist_update_cb)(struct vlist_tree *tree,
-				struct vlist_node *node_new,
-				struct vlist_node *node_old);
-
-struct vlist_tree {
-	struct avl_tree avl;
-
-	vlist_update_cb update;
-	bool keep_old;
-	bool no_delete;
-
-	int version;
-};
-
-struct vlist_node {
-	struct avl_node avl;
-	int version;
-};
-
-void vlist_init(struct vlist_tree *tree, avl_tree_comp cmp, vlist_update_cb update);
-
-#define vlist_find(tree, name, element, node_member) \
-	avl_find_element(&(tree)->avl, name, element, node_member.avl)
-
-static inline void vlist_update(struct vlist_tree *tree)
-{
-	tree->version++;
-}
-
-void vlist_add(struct vlist_tree *tree, struct vlist_node *node, void *key);
-void vlist_delete(struct vlist_tree *tree, struct vlist_node *node);
-void vlist_flush(struct vlist_tree *tree);
-void vlist_flush_all(struct vlist_tree *tree);
-
-#define vlist_for_each_element(tree, element, node_member) \
-	avl_for_each_element(&(tree)->avl, element, node_member.avl)
-
 
 struct vlist_simple_tree {
 	struct list_head list;
