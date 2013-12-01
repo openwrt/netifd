7b0bd859249ee2c8af63f159534d2944d969056d
Felix Fietkau <nbd@openwrt.org>
2013-12-01 21:47:18 +0100

    wireless: fix process handling if the pid exe is a symlink
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- scripts/netifd-wireless.sh
+++ scripts/netifd-wireless.sh
@@ -158,9 +158,13 @@ _wireless_set_data() {
 
 _wireless_add_process() {
 	_wdev_notify_init $CMD_PROCESS_ADD
+	local exe="$2"
+	[ -L "$exe" ] && exe="$(readlink -f "$exe")"
 	json_add_int pid "$1"
-	json_add_string exe "$2"
+	json_add_string exe "$exe"
 	[ -n "$3" ] && json_add_boolean required 1
+	exe2="$(readlink -f /proc/$pid/exe)"
+	[ "$exe" = "$exe2" ] && echo "WARNING (wireless_add_process): executable path $exe does not match process $1 path ($exe2)"
 	_wdev_notify
 }
 
