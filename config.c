bec8a74c53ec900acb0fb3e0c997393dc57f1263
Felix Fietkau <nbd@openwrt.org>
2014-10-07 14:10:06 +0200

    config: define _GNU_SOURCE for asprintf
    Signed-off-by: Felix Fietkau <nbd@openwrt.org>

--- config.c
+++ config.c
@@ -11,6 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+#define _GNU_SOURCE
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
