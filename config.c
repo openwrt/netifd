258b79f7a912abc84a3066c90266d8835597362e
Hans Dedecker <dedeckeh@gmail.com>
2015-02-02 16:11:23 +0100

    netifd: Remove unnecessary default_config check in config_parse_interface
    No need to check default_config twice as it's already checked in config_parse_interface
    
    Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

--- config.c
+++ config.c
@@ -136,7 +136,7 @@ config_parse_interface(struct uci_section *s, bool alias)
 	if (blob_len(b.head) == 0)
 		return;
 
-	if (iface->device_config && dev->default_config)
+	if (iface->device_config)
 		device_set_config(dev, dev->type, b.head);
 	return;
 error_free_config:
