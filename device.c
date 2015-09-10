openwrt/netifd

commit 3224b809c359107b04c067cc00e652729030c735

device: Don't call set_state for external device in device_claim
The function set_state disable is not called for external devices in device_release
which means for external vlan/macvlan devices they won't be deleted.
As a result of this the set_state enable call for external devices by device_claim fails
as vlan/macvlan devices cannot be created since the device already exists in the kernel.
Therefore move the external device check from device_set_state to device_claim so
external vlan/macvlan devices are not created again and can also be external.

Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

device.c
@@ -81,9 +81,6 @@ static int set_device_state(struct device *dev, bool state)
 			return -1;
 	}
 
-	if (dev->external)
-		return 0;
-
 	if (state)
 		system_if_up(dev);
 	else
@@ -324,7 +321,7 @@ void device_broadcast_event(struct device *dev, enum device_event ev)
 int device_claim(struct device_user *dep)
 {
 	struct device *dev = dep->dev;
-	int ret;
+	int ret = 0;
 
 	if (dep->claimed)
 		return 0;
@@ -335,7 +332,9 @@ int device_claim(struct device_user *dep)
 		return 0;
 
 	device_broadcast_event(dev, DEV_EVENT_SETUP);
-	ret = dev->set_state(dev, true);
+	if (!dev->external)
+		ret = dev->set_state(dev, true);
+
 	if (ret == 0)
 		device_broadcast_event(dev, DEV_EVENT_UP);
 	else {

commit 001100fd10fb799572e21551b4f9bdad861a1b4f

device: Resolve ifindex for external claimed devices
Fixes regression issues introduced by commit 3224b80 as external (PPP)
device ifindex was not in sync with kernel device ifindex due to re-creation
of the device by the PPP daemon

Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

device.c
@@ -74,15 +74,14 @@ void device_unlock(void)
 static int set_device_state(struct device *dev, bool state)
 {
 	if (state) {
-		/* Set ifindex for all devices being enabled so a valid  */
+		/* Get ifindex for all devices being enabled so a valid  */
 		/* ifindex is in place avoiding possible race conditions */
 		device_set_ifindex(dev, system_if_resolve(dev));
 		if (!dev->ifindex)
 			return -1;
-	}
 
-	if (state)
 		system_if_up(dev);
+	}
 	else
 		system_if_down(dev);
 
@@ -332,7 +331,13 @@ int device_claim(struct device_user *dep)
 		return 0;
 
 	device_broadcast_event(dev, DEV_EVENT_SETUP);
-	if (!dev->external)
+	if (dev->external) {
+		/* Get ifindex for external claimed devices so a valid   */
+		/* ifindex is in place avoiding possible race conditions */
+		device_set_ifindex(dev, system_if_resolve(dev));
+		if (!dev->ifindex)
+			ret = -1;
+	} else
 		ret = dev->set_state(dev, true);
 
 	if (ret == 0)