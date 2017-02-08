6228d0f21b2dcf20d7b2223c43f8b90b9b51bc4d
Günther Kelleter <guenther.kelleter@devolo.de>
2017-02-08 16:47:22 +0100

    wireless: fix _wireless_add_process
    The pid is in $1, not $pid.
    Use proper test condition for nonmatching exe warning.
    
    Signed-off-by: Günther Kelleter <guenther.kelleter@devolo.de>

--- scripts/netifd-wireless.sh
+++ scripts/netifd-wireless.sh
@@ -163,8 +163,8 @@ _wireless_add_process() {
 	json_add_int pid "$1"
 	json_add_string exe "$exe"
 	[ -n "$3" ] && json_add_boolean required 1
-	exe2="$(readlink -f /proc/$pid/exe)"
-	[ "$exe" = "$exe2" ] && echo "WARNING (wireless_add_process): executable path $exe does not match process $1 path ($exe2)"
+	exe2="$(readlink -f /proc/$1/exe)"
+	[ "$exe" != "$exe2" ] && echo "WARNING (wireless_add_process): executable path $exe does not match process $1 path ($exe2)"
 	_wdev_notify
 }
 
