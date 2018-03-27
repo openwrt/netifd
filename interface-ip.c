openwrt/netifd

commit 1f5a29c3de6e3fec5883796ee772e25d56db6a69

ip: do not add local routes for host dependencies
This avoids creating invalid routes in cases where another daemon is
handling local routes for an interface, e.g. on mesh interfaces

Signed-off-by: Felix Fietkau <nbd@nbd.name>

interface-ip.c
@@ -262,6 +262,7 @@ interface_ip_add_target_route(union if_addr *addr, bool v6, struct interface *if
 		}
 	}
 
+done:
 	if (!r_next) {
 		free(route);
 		return NULL;
@@ -272,8 +273,6 @@ interface_ip_add_target_route(union if_addr *addr, bool v6, struct interface *if
 	route->mtu = r_next->mtu;
 	route->metric = r_next->metric;
 	route->table = r_next->table;
-
-done:
 	route->iface = iface;
 	if (defaultroute_target)
 		free(route);

commit 9c8d7816fc5e966b0c0efab9c1234734e0c5c254

netifd: return the interface for locally addressable host dependencies (FS#1452)
Fixes an issue where interfaces with host dependencies that resolve to a
local subnet stay down.

Fixes: 1f5a29c3de6e ("ip: do not add local routes for host dependencies")
Signed-off-by: Felix Fietkau <nbd@nbd.name>

interface-ip.c
@@ -245,7 +245,7 @@ interface_ip_add_target_route(union if_addr *addr, bool v6, struct interface *if
 	if (iface) {
 		/* look for locally addressable target first */
 		if (interface_ip_find_addr_target(iface, addr, v6))
-			goto done;
+			return iface;
 
 		/* do not stop at the first route, let the lookup compare
 		 * masks to find the best match */
@@ -254,7 +254,7 @@ interface_ip_add_target_route(union if_addr *addr, bool v6, struct interface *if
 		vlist_for_each_element(&interfaces, iface, node) {
 			/* look for locally addressable target first */
 			if (interface_ip_find_addr_target(iface, addr, v6))
-				goto done;
+				return iface;
 
 			/* do not stop at the first route, let the lookup compare
 			 * masks to find the best match */
@@ -262,7 +262,6 @@ interface_ip_add_target_route(union if_addr *addr, bool v6, struct interface *if
 		}
 	}
 
-done:
 	if (!r_next) {
 		free(route);
 		return NULL;