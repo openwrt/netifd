openwrt/netifd

commit 3c8ac1c94d3be039c30a603082198768a82f4732

netifd: fix wpa mixed mode matching
Change wpa mixed mode matching to not accidently catch wep+mixed.

All documented cases have the character between {wpa,psk} and mixed as a
'-' but no need to break things which were working, so preserve the *
case.

Reported-by: "Rick Farina (Zero_Chaos)" <zerochaos@gentoo.org>
[Allow "psk-mixed" to be prefixed, to align with the *psk2* and *psk* cases,
 slightly reword subject and commit message.]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

scripts/netifd-wireless.sh
@@ -214,7 +214,7 @@ wireless_vif_parse_encryption() {
 		wpa2*|*psk2*)
 			wpa=2
 		;;
-		*mixed*)
+		wpa*mixed*|psk*mixed*)
 			wpa=3
 		;;
 		wpa*|*psk*)

commit 94e156f90a58e0d4bea1248bd6088cf92192cdc1

scripts: fix previous commit
Actually change the glob pattern as described in the previous commit.

Fixes: 3c8ac1c ("netifd: fix wpa mixed mode matching")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

scripts/netifd-wireless.sh
@@ -214,7 +214,7 @@ wireless_vif_parse_encryption() {
 		wpa2*|*psk2*)
 			wpa=2
 		;;
-		wpa*mixed*|psk*mixed*)
+		wpa*mixed*|*psk*mixed*)
 			wpa=3
 		;;
 		wpa*|*psk*)