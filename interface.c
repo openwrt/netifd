openwrt/netifd

commit a97297d83e427685d316a170b769922083c02794

interface: set interface in TEARDOWN state when checking link state
When launching PROTO_CMD_TEARDOWN in interface_check_state() the interface
was set in IFS_DOWN state. In case an interface is now brought into IFS_SETUP
state in __interface_set_up() it will launch PROTO_CMD_SETUP trying to
bring the proto shell handler in S_SETUP state which will fail as the proto
shell handler is still in the S_TEARDOWN state.

Fix this by setting the interface in IFS_TEARDOWN state when the PROTO_CMD_TEARDOWN
event is launched which will prevent the interface being brought into IFS_SETUP state
as long as it's not in the IFS_DOWN state.

Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

interface.c
@@ -342,11 +342,11 @@ interface_check_state(struct interface *iface)
 	case IFS_UP:
 	case IFS_SETUP:
 		if (!iface->enabled || !link_state) {
-			interface_proto_event(iface->proto, PROTO_CMD_TEARDOWN, false);
+			iface->state = IFS_TEARDOWN;
 			if (iface->dynamic)
 				__set_config_state(iface, IFC_REMOVE);
 
-			mark_interface_down(iface);
+			interface_proto_event(iface->proto, PROTO_CMD_TEARDOWN, false);
 		}
 		break;
 	case IFS_DOWN:

commit bfd4de3666901070d805878e55b02417fef6277c

interface: fix "if-down" hotplug event handling
commit a97297d83e42 ("interface: set interface in TEARDOWN state when checking link state")
broke the if-down hotplug event handling, as the iface->state is now IFS_TEARDOWN when
calling the mark_interface_down() function from the IFPEV_DOWN event.

Fixes: a97297d83e42 ("interface: set interface in TEARDOWN state when checking link state")
Signed-off-by: Martin Schiller <ms@dev.tdt.de>

interface.c
@@ -269,6 +269,7 @@ mark_interface_down(struct interface *iface)
 	iface->state = IFS_DOWN;
 	switch (state) {
 	case IFS_UP:
+	case IFS_TEARDOWN:
 		interface_event(iface, IFEV_DOWN);
 		break;
 	case IFS_SETUP: