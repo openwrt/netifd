openwrt/netifd

commit 516ab774cc16d4b04b3b17a067cbf2649f1adaeb

system-linux: fix race condition on bringing up wireless devices
When wireless devices are added via hotplug before their state is set to up,
adding routes to the device can fail in the kernel.
Since the up state is managed externally, use it as input for netifd's device
present state, so that they are only brought up when ready.

Signed-off-by: Felix Fietkau <nbd@nbd.name>

system-linux.c
@@ -692,34 +692,44 @@ static int system_get_arp_accept(struct device *dev, char *buf, const size_t buf
 			dev->ifname, buf, buf_sz);
 }
 
+#ifndef IFF_LOWER_UP
+#define IFF_LOWER_UP	0x10000
+#endif
+
+static void
+system_device_update_state(struct device *dev, unsigned int flags, unsigned int ifindex)
+{
+	if (dev->type == &simple_device_type) {
+		bool present = ifindex > 0;
+
+		if (dev->external)
+			present = present && (flags & IFF_UP);
+
+		device_set_present(dev, present);
+	}
+	device_set_link(dev, flags & IFF_LOWER_UP ? true : false);
+}
+
 /* Evaluate netlink messages */
 static int cb_rtnl_event(struct nl_msg *msg, void *arg)
 {
 	struct nlmsghdr *nh = nlmsg_hdr(msg);
+	struct ifinfomsg *ifi = NLMSG_DATA(nh);
 	struct nlattr *nla[__IFLA_MAX];
-	int link_state = 0;
-	char buf[10];
+	struct device *dev;
 
 	if (nh->nlmsg_type != RTM_NEWLINK)
-		goto out;
+		return 0;
 
 	nlmsg_parse(nh, sizeof(struct ifinfomsg), nla, __IFLA_MAX - 1, NULL);
 	if (!nla[IFLA_IFNAME])
-		goto out;
+		return 0;
 
-	struct device *dev = device_find(nla_data(nla[IFLA_IFNAME]));
+	dev = device_find(nla_data(nla[IFLA_IFNAME]));
 	if (!dev)
-		goto out;
+		return 0;
 
-	if (!system_get_dev_sysfs("carrier", dev->ifname, buf, sizeof(buf)))
-		link_state = strtoul(buf, NULL, 0);
-
-	if (dev->type == &simple_device_type)
-		device_set_present(dev, true);
-
-	device_set_link(dev, link_state ? true : false);
-
-out:
+	system_device_update_state(dev, ifi->ifi_flags, ifi->ifi_index);
 	return 0;
 }
 
@@ -2092,10 +2102,6 @@ struct if_check_data {
 	int ret;
 };
 
-#ifndef IFF_LOWER_UP
-#define IFF_LOWER_UP	0x10000
-#endif
-
 static int cb_if_check_valid(struct nl_msg *msg, void *arg)
 {
 	struct nlmsghdr *nh = nlmsg_hdr(msg);
@@ -2105,10 +2111,7 @@ static int cb_if_check_valid(struct nl_msg *msg, void *arg)
 	if (nh->nlmsg_type != RTM_NEWLINK)
 		return NL_SKIP;
 
-	if (chk->dev->type == &simple_device_type)
-		device_set_present(chk->dev, ifi->ifi_index > 0 ? true : false);
-	device_set_link(chk->dev, ifi->ifi_flags & IFF_LOWER_UP ? true : false);
-
+	system_device_update_state(chk->dev, ifi->ifi_flags, ifi->ifi_index);
 	return NL_OK;
 }
 

commit 35facc8306f590a7330789ab6d5785c0d43073ef

wireless: fix premature removal of hotplug devices due to down state
When a device is added that isn't up, status toggles can sometimes lead to a
DEV_EVENT_REMOVE event, which causes the device to be removed from an interface
or a bridge.
Fix this by toggling the dev->disabled status instead based on IFF_UP, and
adding a check to bridge/interface code to only permanently remove devices that
are actually gone.

Fixes: 516ab774cc16 ("system-linux: fix race condition on bringing up wireless devices")
Signed-off-by: Felix Fietkau <nbd@nbd.name>

bonding.c
@@ -303,7 +303,7 @@ bonding_port_cb(struct device_user *dep, enum device_event ev)
 		bonding_disable_port(bp, true);
 		break;
 	case DEV_EVENT_REMOVE:
-		if (dep->hotplug) {
+		if (dep->hotplug && !dev->sys_present) {
 			vlist_delete(&bdev->ports, &bp->node);
 			return;
 		}

bridge.c
@@ -794,7 +794,7 @@ bridge_member_cb(struct device_user *dep, enum device_event ev)
 		bridge_disable_member(bm, true);
 		break;
 	case DEV_EVENT_REMOVE:
-		if (dep->hotplug) {
+		if (dep->hotplug && !dev->sys_present) {
 			vlist_delete(&bst->members, &bm->node);
 			return;
 		}

device.c
@@ -880,9 +880,9 @@ void device_cleanup(struct device *dev)
 	device_delete(dev);
 }
 
-static void __device_set_present(struct device *dev, bool state)
+static void __device_set_present(struct device *dev, bool state, bool force)
 {
-	if (dev->present == state)
+	if (dev->present == state && !force)
 		return;
 
 	dev->present = state;
@@ -897,7 +897,7 @@ device_refresh_present(struct device *dev)
 	if (dev->disabled || dev->deferred)
 		state = false;
 
-	__device_set_present(dev, state);
+	__device_set_present(dev, state, false);
 }
 
 void
@@ -937,7 +937,10 @@ void device_set_present(struct device *dev, bool state)
 
 	D(DEVICE, "%s '%s' %s present\n", dev->type->name, dev->ifname, state ? "is now" : "is no longer" );
 	dev->sys_present = state;
-	device_refresh_present(dev);
+	if (!state)
+		__device_set_present(dev, state, true);
+	else
+		device_refresh_present(dev);
 	if (!state)
 		safe_list_for_each(&dev->users, device_release_cb, NULL);
 }

interface.c
@@ -437,7 +437,7 @@ interface_main_dev_cb(struct device_user *dep, enum device_event ev)
 		break;
 	case DEV_EVENT_REMOVE:
 		interface_set_available(iface, false);
-		if (dep->dev && dep->dev->external)
+		if (dep->dev && dep->dev->external && !dep->dev->sys_present)
 			interface_set_main_dev(iface, NULL);
 		break;
 	case DEV_EVENT_UP:

system-linux.c
@@ -700,12 +700,10 @@ static void
 system_device_update_state(struct device *dev, unsigned int flags, unsigned int ifindex)
 {
 	if (dev->type == &simple_device_type) {
-		bool present = ifindex > 0;
-
 		if (dev->external)
-			present = present && (flags & IFF_UP);
+			device_set_disabled(dev, !(flags & IFF_UP));
 
-		device_set_present(dev, present);
+		device_set_present(dev, ifindex > 0);
 	}
 	device_set_link(dev, flags & IFF_LOWER_UP ? true : false);
 }