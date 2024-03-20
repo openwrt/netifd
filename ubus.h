94625c049f80b6ca80105250aeaaff2618a4bb42
Rahul Thakur <rahul.thakur@iopsys.eu>
2024-03-20 11:29:13 +0530

    device: add ubus event for link state change
    Add a ubus event network.device for link state change to
    generate an event when the link goes up and down.
    
    The event looks as follows:
    { "network.device": {"ifname":"eth3","link":"down"} }
    
    Signed-off-by: Rahul Thakur <rahul.thakur@iopsys.eu>

--- ubus.h
+++ ubus.h
@@ -26,5 +26,6 @@ void netifd_ubus_remove_interface(struct interface *iface);
 void netifd_ubus_interface_event(struct interface *iface, bool up);
 void netifd_ubus_interface_notify(struct interface *iface, bool up);
 void netifd_ubus_device_notify(const char *event, struct blob_attr *data, int timeout);
+void netifd_ubus_device_event(struct device *dev, bool state);
 
 #endif
