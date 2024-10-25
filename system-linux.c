885cf9b2a0e15a4c10900e9a567e25ef326402c9
Paul Donald <newtwen+github@gmail.com>
2024-10-25 14:54:23 +0200

    vxlan: add ttlinherit boolean attribute
    Signed-off-by: Paul Donald <newtwen+github@gmail.com>

--- system-linux.c
+++ system-linux.c
@@ -4360,6 +4360,7 @@ static int system_add_vxlan(const char *name, const unsigned int link, struct bl
 	system_vxlan_map_bool_attr(msg, tb_data, IFLA_VXLAN_L3MISS , VXLAN_DATA_ATTR_L3MISS, false);
 	system_vxlan_map_bool_attr(msg, tb_data, IFLA_VXLAN_GBP , VXLAN_DATA_ATTR_GBP, false);
 	system_vxlan_map_bool_attr(msg, tb_data, IFLA_VXLAN_GPE , VXLAN_DATA_ATTR_GPE, false);
+	system_vxlan_map_bool_attr(msg, tb_data, IFLA_VXLAN_TTL_INHERIT , VXLAN_DATA_ATTR_TTL_INHERIT, false);
 
 	if ((cur = tb_data[VXLAN_DATA_ATTR_AGEING])) {
 		uint32_t ageing = blobmsg_get_u32(cur);
