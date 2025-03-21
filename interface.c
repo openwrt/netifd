8cf7493119fad82a2c5a27ccef6934fb0a787934
Felix Fietkau <nbd@nbd.name>
2025-03-21 12:13:51 +0100

    wireless: replace with ucode scripts
    Wireless configuration is a lot easier to deal with in a high level language
    like ucode. Replace lots of C code with vastly simpler scripts.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- interface.c
+++ interface.c
@@ -23,9 +23,9 @@
 #include "interface-ip.h"
 #include "proto.h"
 #include "ubus.h"
+#include "ucode.h"
 #include "config.h"
 #include "system.h"
-#include "wireless.h"
 
 struct vlist_tree interfaces;
 static LIST_HEAD(iface_all_users);
@@ -1137,7 +1137,7 @@ interface_set_up(struct interface *iface)
 	const char *error = NULL;
 
 	iface->autostart = true;
-	wireless_check_network_enabled();
+	netifd_ucode_check_network_enabled();
 
 	if (iface->state != IFS_DOWN)
 		return;
@@ -1170,7 +1170,7 @@ interface_set_down(struct interface *iface)
 			__interface_set_down(iface, false);
 	} else {
 		iface->autostart = false;
-		wireless_check_network_enabled();
+		netifd_ucode_check_network_enabled();
 		__interface_set_down(iface, false);
 	}
 }
