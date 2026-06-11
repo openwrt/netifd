openwrt/netifd

commit d9872db72eb63e9fa454446756e10fc7c52effc5

interface: fix removal of dynamic interfaces
Set config state to remove for dynamic interfaces in the following cases :
-interface is set as not available
-interface is set as down
-interface is set as having no link state
This will trigger an interface delete upon the next call of interface_handle_config_change

Before this change you could end up with lingering inactive dynamic
interfaces in case the aliased interface went down as before a dynamic
interface was only removed when set down via ubus

Signed-off-by: Hans Dedecker <dedeckeh@gmail.com>

interface.c
@@ -284,6 +284,12 @@ mark_interface_down(struct interface *iface)
 	system_flush_routes();
 }
 
+static inline void
+__set_config_state(struct interface *iface, enum interface_config_state s)
+{
+	iface->config_state = s;
+}
+
 static void
 __interface_set_down(struct interface *iface, bool force)
 {
@@ -292,6 +298,9 @@ __interface_set_down(struct interface *iface, bool force)
 	case IFS_UP:
 	case IFS_SETUP:
 		iface->state = IFS_TEARDOWN;
+		if (iface->dynamic)
+			__set_config_state(iface, IFC_REMOVE);
+
 		if (state == IFS_UP)
 			interface_event(iface, IFEV_DOWN);
 
@@ -334,6 +343,9 @@ interface_check_state(struct interface *iface)
 	case IFS_SETUP:
 		if (!iface->enabled || !link_state) {
 			interface_proto_event(iface->proto, PROTO_CMD_TEARDOWN, false);
+			if (iface->dynamic)
+				__set_config_state(iface, IFC_REMOVE);
+
 			mark_interface_down(iface);
 		}
 		break;
@@ -697,8 +709,6 @@ interface_handle_config_change(struct interface *iface)
 	}
 	if (iface->autostart)
 		interface_set_up(iface);
-	else if (iface->dynamic)
-		set_config_state(iface, IFC_REMOVE);
 }
 
 static void
@@ -1120,7 +1130,7 @@ interface_start_pending(void)
 static void
 set_config_state(struct interface *iface, enum interface_config_state s)
 {
-	iface->config_state = s;
+	__set_config_state(iface, s);
 	if (iface->state == IFS_DOWN)
 		interface_handle_config_change(iface);
 	else

commit 186f0e115e1fd3e032ffb44b270f6566b8b30959

interface: defer freeing of removed interfaces
interface_do_remove() freed the interface synchronously. For a dynamic
interface with an immediate protocol (none, static), removal can be
triggered from deep inside a callback chain that keeps using the
interface afterwards: deleting the device of an interface that is up
runs device_broadcast_event -> interface_main_dev_cb ->
interface_set_available(false) -> __interface_set_down ->
interface_proto_event. PROTO_FLAG_IMMEDIATE protocols deliver IFPEV_DOWN
synchronously from there, and its handler ends in
interface_handle_config_change -> interface_do_remove -> interface_free.
While unwinding, __interface_set_down then calls
interface_flush_state(iface) and interface_main_dev_cb dereferences
dep->dev, both reading freed memory.

Keep the teardown, cleanup and deregistration synchronous, but defer the
final free to a uloop timeout. remove_timer can be reused for this as
interface_cleanup() has already cancelled it at that point. Callbacks
unwinding through the cleaned-up interface then only see NULLed device
users, which they handle already.

Fixes: d9872db72eb6 ("interface: fix removal of dynamic interfaces")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

interface.c
@@ -728,13 +728,22 @@ void interface_free(struct interface *iface)
 }
 
 static void
+interface_free_cb(struct uloop_timeout *timeout)
+{
+	struct interface *iface = container_of(timeout, struct interface, remove_timer);
+
+	interface_free(iface);
+}
+
+static void
 interface_do_remove(struct interface *iface)
 {
 	interface_event(iface, IFEV_FREE);
 	interface_cleanup(iface);
 	netifd_ubus_remove_interface(iface);
 	avl_delete(&interfaces.avl, &iface->node.avl);
-	interface_free(iface);
+	iface->remove_timer.cb = interface_free_cb;
+	uloop_timeout_set(&iface->remove_timer, 1);
 }
 
 static void