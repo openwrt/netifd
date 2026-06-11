openwrt/netifd

commit d93126d3b91ea9e3831b9fd0b5318989f82daebe

interface: allow renaming interface when moving to jail netns
Introduce jail_ifname option to define the name of a Linux network
interface when moved into a jail's network namespace.
This is useful for containers which expect the network interface to
have a specific name (eg. 'host0' in case of systemd).
While at it, clean-up and fix bugs in jail interface up/down routines.

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

system-linux.c
@@ -843,14 +843,19 @@ int system_bridge_delif(struct device *bridge, struct device *dev)
 	return system_bridge_if(bridge->ifname, dev, SIOCBRDELIF, NULL);
 }
 
-int system_if_resolve(struct device *dev)
+static int system_ifname_resolve(const char *ifname)
 {
 	struct ifreq ifr;
-	strncpy(ifr.ifr_name, dev->ifname, sizeof(ifr.ifr_name) - 1);
+	strncpy(ifr.ifr_name, ifname, sizeof(ifr.ifr_name) - 1);
 	if (!ioctl(sock_ioctl, SIOCGIFINDEX, &ifr))
 		return ifr.ifr_ifindex;
 	else
 		return 0;
+}
+
+int system_if_resolve(struct device *dev)
+{
+	return system_ifname_resolve(dev->ifname);
 }
 
 static int system_if_flags(const char *ifname, unsigned add, unsigned rem)
@@ -1246,21 +1251,23 @@ nla_put_failure:
 	return -ENOMEM;
 }
 
-int system_link_netns_move(const char *ifname, int netns_fd)
+int system_link_netns_move(const char *ifname, int netns_fd, const char *target_ifname)
 {
 	struct nl_msg *msg;
 	struct ifinfomsg iim = {
 		.ifi_family = AF_UNSPEC,
-		.ifi_index = 0,
 	};
 
+	iim.ifi_index = system_ifname_resolve(ifname);
 	msg = nlmsg_alloc_simple(RTM_NEWLINK, NLM_F_REQUEST);
 
 	if (!msg)
 		return -1;
 
 	nlmsg_append(msg, &iim, sizeof(iim), 0);
-	nla_put_string(msg, IFLA_IFNAME, ifname);
+	if (target_ifname)
+		nla_put_string(msg, IFLA_IFNAME, target_ifname);
+
 	nla_put_u32(msg, IFLA_NET_NS_FD, netns_fd);
 	return system_rtnl_call(msg);
 }

interface.c
@@ -34,6 +34,7 @@ enum {
 	IFACE_ATTR_PROTO,
 	IFACE_ATTR_AUTO,
 	IFACE_ATTR_JAIL,
+	IFACE_ATTR_JAIL_IFNAME,
 	IFACE_ATTR_DEFAULTROUTE,
 	IFACE_ATTR_PEERDNS,
 	IFACE_ATTR_DNS,
@@ -58,6 +59,7 @@ static const struct blobmsg_policy iface_attrs[IFACE_ATTR_MAX] = {
 	[IFACE_ATTR_IFNAME] = { .name = "ifname", .type = BLOBMSG_TYPE_STRING },
 	[IFACE_ATTR_AUTO] = { .name = "auto", .type = BLOBMSG_TYPE_BOOL },
 	[IFACE_ATTR_JAIL] = { .name = "jail", .type = BLOBMSG_TYPE_STRING },
+	[IFACE_ATTR_JAIL_IFNAME] = { .name = "jail_ifname", .type = BLOBMSG_TYPE_STRING },
 	[IFACE_ATTR_DEFAULTROUTE] = { .name = "defaultroute", .type = BLOBMSG_TYPE_BOOL },
 	[IFACE_ATTR_PEERDNS] = { .name = "peerdns", .type = BLOBMSG_TYPE_BOOL },
 	[IFACE_ATTR_METRIC] = { .name = "metric", .type = BLOBMSG_TYPE_INT32 },
@@ -686,6 +688,8 @@ interface_do_free(struct interface *iface)
 	avl_delete(&interfaces.avl, &iface->node.avl);
 	if (iface->jail)
 		free(iface->jail);
+	if (iface->jail_ifname)
+		free(iface->jail_ifname);
 
 	free(iface);
 }
@@ -899,6 +903,10 @@ interface_alloc(const char *name, struct blob_attr *config, bool dynamic)
 		iface->jail = strdup(blobmsg_get_string(cur));
 		iface->autostart = false;
 	}
+
+	iface->jail_ifname = NULL;
+	if ((cur = tb[IFACE_ATTR_JAIL_IFNAME]))
+		iface->jail_ifname = strdup(blobmsg_get_string(cur));
 
 	return iface;
 }
@@ -1163,15 +1171,21 @@ interface_start_jail(const char *jail, const pid_t netns_pid)
 		if (!iface->jail || strcmp(iface->jail, jail))
 			continue;
 
-		system_link_netns_move(iface->ifname, netns_fd);
+		system_link_netns_move(iface->ifname, netns_fd, iface->jail_ifname);
 	}
+
+	close(netns_fd);
 
 	pr = fork();
 	if (pr) {
 		waitpid(pr, &wstatus, WUNTRACED | WCONTINUED);
-		close(netns_fd);
 		return;
 	}
+
+	/* child process */
+	netns_fd = system_netns_open(netns_pid);
+	if (netns_fd < 0)
+		return;
 
 	system_netns_set(netns_fd);
 	system_init();
@@ -1179,8 +1193,19 @@ interface_start_jail(const char *jail, const pid_t netns_pid)
 		if (!iface->jail || strcmp(iface->jail, jail))
 			continue;
 
+		/*
+		 * The interface has already been renamed and is inside target
+		 * namespace, hence overwrite ifname with jail_ifname for
+		 * interface_set_up().
+		 * We are inside a fork which got it's own copy of the interfaces
+		 * list, so we can mess with it :)
+		 */
+		iface->ifname = iface->jail_ifname;
+		interface_do_reload(iface);
 		interface_set_up(iface);
 	}
+
+	close(netns_fd);
 	_exit(0);
 }
 
@@ -1190,23 +1215,23 @@ interface_stop_jail(const char *jail, const pid_t netns_pid)
 	struct interface *iface;
 	int netns_fd, root_netns;
 	int wstatus;
+	pid_t parent_pid = getpid();
 	pid_t pr = 0;
-
-	netns_fd = system_netns_open(netns_pid);
-	if (netns_fd < 0)
-		return;
-
-	root_netns = system_netns_open(getpid());
-	if (root_netns < 0)
-		return;
 
 	pr = fork();
 	if (pr) {
 		waitpid(pr, &wstatus, WUNTRACED | WCONTINUED);
-		close(netns_fd);
-		close(root_netns);
 		return;
 	}
+
+	/* child process */
+	root_netns = system_netns_open(parent_pid);
+	if (root_netns < 0)
+		return;
+
+	netns_fd = system_netns_open(netns_pid);
+	if (netns_fd < 0)
+		return;
 
 	system_netns_set(netns_fd);
 	system_init();
@@ -1215,8 +1240,11 @@ interface_stop_jail(const char *jail, const pid_t netns_pid)
 			continue;
 
 		interface_set_down(iface);
-		system_link_netns_move(iface->ifname, root_netns);
+		system_link_netns_move(iface->jail_ifname, root_netns, iface->ifname);
 	}
+
+	close(root_netns);
+	close(netns_fd);
 	_exit(0);
 }
 
@@ -1334,6 +1362,11 @@ interface_change_config(struct interface *if_old, struct interface *if_new)
 	if_old->jail = if_new->jail;
 	if (if_old->jail)
 		if_old->autostart = false;
+
+	if (if_old->jail_ifname)
+		free(if_old->jail_ifname);
+
+	if_old->jail_ifname = if_new->jail_ifname;
 
 	if_old->ifname = if_new->ifname;
 	if_old->parent_ifname = if_new->parent_ifname;

interface.h
@@ -109,6 +109,7 @@ struct interface {
 	const char *name;
 	const char *ifname;
 	char *jail;
+	char *jail_ifname;
 	int netns_fd;
 
 	bool available;

system-dummy.c
@@ -54,9 +54,9 @@ int system_bridge_delif(struct device *bridge, struct device *dev)
 	return 0;
 }
 
-int system_link_netns_move(const char *ifname, int netns_fd)
+int system_link_netns_move(const char *ifname, int netns_fd, const char *target_ifname)
 {
-	D(SYSTEM, "ip link %s netns %d\n", ifname, netns_fd);
+	D(SYSTEM, "ip link dev %s name %s netns %d\n", ifname, target_ifname, netns_fd);
 	return 0;
 }
 

system.h
@@ -243,7 +243,7 @@ void system_fd_set_cloexec(int fd);
 
 int system_update_ipv6_mtu(struct device *dev, int mtu);
 
-int system_link_netns_move(const char *ifname, const pid_t target_ns);
+int system_link_netns_move(const char *ifname, const pid_t target_ns, const char *target_ifname);
 int system_netns_open(const pid_t target_ns);
 int system_netns_set(int netns_fd);
 

ubus.c
@@ -765,6 +765,9 @@ netifd_dump_status(struct interface *iface)
 	if (iface->jail)
 		blobmsg_add_string(&b, "jail", iface->jail);
 
+	if (iface->jail_ifname)
+		blobmsg_add_string(&b, "jail_ifname", iface->jail_ifname);
+
 	if (iface->state == IFS_UP) {
 		if (iface->updated) {
 			a = blobmsg_open_array(&b, "updated");

commit e150cec47dc02283a9c1b41875dd85aee52f8899

system-linux: refuse netns move of an unresolved device
system_link_netns_move() built its RTM_NEWLINK message with whatever
system_if_resolve() returned for the source device. When the device does
not exist (e.g. a jailed interface whose veth has not been created yet),
system_if_resolve() returns 0, and the message goes out with
ifi_index = 0 while IFLA_IFNAME is set to target_ifname (the jail_device
name).

The kernel's __rtnl_newlink() then selects the target device by name when
ifi_index is 0 (net/core/rtnetlink.c: rtnl_dev_get -> __dev_get_by_name),
so the move operates on whatever host device happens to be named like the
jail_device. With a jail_device of "eth0" this moves the host's real eth0
into the container's network namespace, knocking the host off the
network.

Bail out when the source device cannot be resolved to a real ifindex so
the request is never sent with a zero index.

Fixes: d93126d3b91e ("interface: allow renaming interface when moving to jail netns")
Signed-off-by: Daniel Golle <daniel@makrotopia.org>

system-linux.c
@@ -1670,6 +1670,9 @@ int system_link_netns_move(struct device *dev, int netns_fd, const char *target_
 		return -1;
 
 	index = system_if_resolve(dev);
+	if (index <= 0)
+		return -1;
+
 	msg = __system_ifinfo_msg(AF_UNSPEC, index, target_ifname, RTM_NEWLINK, 0);
 	if (!msg)
 		return -1;